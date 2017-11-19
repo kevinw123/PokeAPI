5ae2cfc9ca6df752f4d8b762e98f4bef545b2da2
Kevin Wong <kevinwong_895@hotmail.com>
2017-11-18 22:18:35 -0800

    Reading json data from file

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
 from flask import Flask, jsonify
+import json
+from pprint import pprint
 
 app = Flask(__name__)
 
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    data = json.load(open('data.json'))
+    pprint(data)
+    return jsonify(data)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.before_first_request
+def startup():
+    print("First Run")
 
 if __name__ == '__main__':
     app.run(debug=True)
